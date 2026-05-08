757f82cd8db285fa4f684b24eb9ba0e3d1ecee67
html-ramu <ramuapplab@gmail.com>
2026-05-08 18:46:19 +0530

    Added missing exports

--- firebase.js
+++ firebase.js
@@ -3,10 +3,15 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0P0G9WZUurQAWs7Bj8fob5QlKwrN3csQ",
+  apiKey: "AIzaSyA0P0G9WZUurQAws7Bj8fob5QLkwrN3csQ",
   authDomain: "tool-box-7668e.firebaseapp.com",
   projectId: "tool-box-7668e",
   storageBucket: "tool-box-7668e.firebasestorage.app",
   messagingSenderId: "776985051892",
   appId: "1:776985051892:web:aaf3b55ba95bf04589d4a1"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
