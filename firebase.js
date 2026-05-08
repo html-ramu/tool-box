8c393e9e4f368f7aa35cc0fdc7bd981c728a4762
html-ramu <ramuapplab@gmail.com>
2026-05-08 18:32:25 +0530

    Added correct API key and fixed CSS

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyA0P0G9WZUurQAws7Bj8fob5QLkwrN3csQ",
+    authDomain: "tool-box-7668e.firebaseapp.com",
+    projectId: "tool-box-7668e",
+    storageBucket: "tool-box-7668e.firebasestorage.app",
+    messagingSenderId: "776985051892",
+    appId: "1:776985051892:web:aaf3b55ba95bf04589d4a1"
 };
 
 const app = initializeApp(firebaseConfig);
